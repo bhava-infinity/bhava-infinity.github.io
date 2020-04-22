4372b66c03080135dabdae558466d5e637d9cf73
bhava-infinity <64151386+bhava-infinity@users.noreply.github.com>
2020-04-22 20:44:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var w = (c.width = window.innerWidth),
   hw = w / 2, // half-width
   hh = h / 2,
   opts = {
-    strings: ["HAPPY", "BIRTHDAY!", "Dinesh"],
+    strings: ["HAPPY", "BIRTHDAY!", "DINESH"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
